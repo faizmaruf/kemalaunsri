e2b343218d785b67103c5a92bc47ac5f8b73ff7f
Faiz Ma'ruf <faiz.maruf8888@gmail.com>
2020-08-13 09:40:04 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/kemalaunsri/';
-// $config['base_url'] = 'http://www.kemalaunsri.org/';
+// $config['base_url'] = 'http://localhost:8888/kemalaunsri/';
+$config['base_url'] = 'http://www.kemalaunsri.org/';
 
 /*
 |--------------------------------------------------------------------------
